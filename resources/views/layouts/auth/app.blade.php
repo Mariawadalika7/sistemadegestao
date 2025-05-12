f56023f3ac18bec5262764ef60c04227f1cabb75
cirilocanganjo <cirilocanganjolr@gmail.com>
2025-05-12 10:21:45 +0100

    script and asset directives added on auth-component

--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Sign In / Sign Up</title>
+  @assets
+  @endassets
   <livewire:styles />
 </head>
 <body>
@@ -11,6 +13,8 @@
             {{ $slot }}  
             <livewire:scripts />      
     </div>
+    @script
+    @endscript
 </body>
 </html>
 
