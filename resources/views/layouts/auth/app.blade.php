5e269299a2967eb812e7c9a9cc295d51e57f8b70
cirilocanganjo <cirilocanganjolr@gmail.com>
2025-05-12 10:10:31 +0100

    bug fixed on loading livewire files directives added

--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -4,12 +4,13 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Sign In / Sign Up</title>
-
+  <livewire:styles />
 </head>
 <body>
-        <div>
-            {{ $slot }}           
-        </div>
+    <div>
+            {{ $slot }}  
+            <livewire:scripts />      
+    </div>
 </body>
 </html>
 
--- resources/views/livewire/auth/auth-component.blade.php
+++ resources/views/livewire/auth/auth-component.blade.php
@@ -1,17 +1,19 @@
 <div>
+    <form wire:submit='signIn'>
+            <div>
+                <input wire:model='email' type='email' />
+                @error('email')<span style='color:red;'>{{$message}}</span>@enderror
+            </div>
+        
+            <div>
+                <input wire:model='password'  type='password' />
+                @error('password')<span style='color:red;'>{{$message}}</span>@enderror
+            </div>
+        
+            <div>
+                <button type='submit' >Enter</button>
+            </div>
+    </form>
 
-    <div>
-        <input wire:model='email' type='email' />
-        @error('email')<span style='color:red;'>{{$message}}</span>@enderror
-    </div>
-
-    <div>
-        <input wire:model='password'  type='password' />
-        @error('password')<span style='color:red;'>{{$message}}</span>@enderror
-    </div>
-
-    <div>
-        <button wire:click='signIn'>Enter</button>
-    </div>
 
 </div>
