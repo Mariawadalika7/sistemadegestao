02558b1e8a22820d27e42736b31e343a816aac5a
cirilocanganjo <cirilocanganjolr@gmail.com>
2025-05-14 12:15:09 +0100

    validations on sign up component done

--- app/Livewire/Auth/SignUpComponent.php
+++ app/Livewire/Auth/SignUpComponent.php
@@ -2,14 +2,32 @@
 
 namespace App\Livewire\Auth;
 use Livewire\Attributes\Layout;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 use Livewire\Component;
 
 class SignUpComponent extends Component
 {
     
    #[Layout('layouts.auth.app')] 
+   public $fullname,$birthday,$phone_number,$email,$address,$password,$password_confirmation;
+   protected $rules = ['fullname' =>'required', 'birthday' =>'required', 'phone_number' =>'required', 'email' =>'required|unique:users', 'address' =>'required', 'password' =>'required', 'password_confirmation' =>'required|same:password'];
+   protected $messages = ['fullname.required' =>'Campo obrigatório', 'birthday.required' =>'Campo obrigatório' ,'phone_number.required' =>'Campo obrigatório', 'email.required' =>'Campo obrigatório', 'email.unique' =>'O email já existe', 'address.required' =>'Campo obrigatório', 'password.required' =>'Campo obrigatório', 'password_confirmation.required' =>'Campo obrigatório', 'password_confirmation.same' =>'O campo senha e confirmar devem corresponder'];
+
     public function render()
     {
         return view('livewire.auth.sign-up-component');
+    }
+
+    public function sign_up () {
+        $this->validate();
+        try {
+        } catch (\Throwable $th) {
+         LivewireAlert::title('Erro')
+            ->text('erro: ' .$th->getMessage())
+            ->error()
+            ->withConfirmButton()
+            ->confirmButtonText('Close')
+            ->show();
+        }
     }
 }
--- resources/views/livewire/auth/sign-up-component.blade.php
+++ resources/views/livewire/auth/sign-up-component.blade.php
@@ -7,24 +7,44 @@
         </div>
         <h1>Crie sua conta</h1>
         <p class="subtitle">Preencha os campos abaixo para se cadastrar</p>
-        <form method="POST" action="">
-            
+        
+        <main>
             <!-- Name -->
             <div class="form-group">
                 <label for="name">Nome</label>
-                <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name">
-                @error('name')
-                    <div class="error-message">{{ $message }}</div>
-                @enderror
+                <input id="name" type="text" wire:model="fullname"  required autofocus autocomplete="name">
+                @error('fullname')<div class="error-message">{{ $message }}</div> @enderror                    
+               
             </div>
+
+             <div class="form-group">
+                <label for="name">Data de nascimento</label>
+                <input id="name" type="date" wire:model="birthday" required autofocus autocomplete="name">
+                @error('birthday') <div class="error-message">{{ $message }}</div> @enderror
+            </div>                  
+               
+
+            <div class="form-group">
+                <label for="name">Número de telefone</label>
+                <input id="name" type="text" wire:model='phone_number' required autofocus autocomplete="name">
+                @error('phone_number')<div class="error-message">{{ $message }}</div>@enderror
+            </div>                   
+                
+
+            <div class="form-group">
+                <label for="name">Endereço</label>
+                <input id="name" type="text" wire:model='address' required autofocus autocomplete="name">
+                @error('address') <div class="error-message">{{ $message }}</div>@enderror
+            </div>      
+            
+
             <!-- Email Address -->
             <div class="form-group">
                 <label for="email">Email</label>
-                <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="username">
-                @error('email')
-                    <div class="error-message">{{ $message }}</div>
-                @enderror
-            </div>
+                <input id="email" type="email" wire:model="email" required autocomplete="username">
+                @error('email')<div class="error-message">{{ $message }}</div> @enderror
+            </div>                    
+               
             <!-- Password -->
             <div class="form-group">
                 <label for="password">Senha</label>
@@ -36,15 +56,13 @@
             <!-- Confirm Password -->
             <div class="form-group">
                 <label for="password_confirmation">Confirmar Senha</label>
-                <input id="password_confirmation" type="password" name="password_confirmation" required autocomplete="new-password">
+                <input id="password_confirmation" type="password" wire:model="password_confirmation" required autocomplete="new-password">
                 @error('password_confirmation')
                     <div class="error-message">{{ $message }}</div>
                 @enderror
             </div>
 
-            <button type="submit" class="register-button">
-                Cadastrar
-            </button>
+            <button wire:click='sign_up' class="register-button">Cadastrar</button>      
 
             <div class="login-link">
                 Já tem uma conta?
@@ -52,6 +70,7 @@
                     Faça login
                 </a>
             </div>
-        </form>
+        </main>
+
     </div>
 </div>
